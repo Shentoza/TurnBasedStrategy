3709cd382945e1d050169911de304b608d1eaf68
Tibaos Fuckboi <mccloskey117@yahoo.de>
2015-11-13 13:11:29 +0100

    Grid done

---
+++ TurnBasedStrategy/Assets/Assets/Scripts/Cell.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+	public GameObject upperNeighbour;
+	public GameObject leftNeighbour;
+	public GameObject rightNeightbour;
+	public GameObject lowerNeighbour;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setNeighbours(GameObject u, GameObject le, GameObject r, GameObject lo)
+	{
+		upperNeighbour = u;
+		leftNeighbour = le;
+		rightNeightbour = r;
+		lowerNeighbour = lo;
+
+	}
+}
