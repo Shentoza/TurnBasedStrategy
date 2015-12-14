6880d38cf553165cff69b0e42bfb1ac8d4a50aad
Skeletti <schreiber.daniel87@gmail.com>
2015-12-14 15:25:48 +0100

    AttributeComponent; ManagerSystem

---
+++ TurnBasedStrategy/Assets/Scripts/AttributeComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttributeComponent : MonoBehaviour {
+
+    public int hp;
+    public int ap;
+    public int movementRange;
+    public float accuracy;
+    public float attackRange;
+    public GameObject weapon;
+    public GameObject[] items;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
