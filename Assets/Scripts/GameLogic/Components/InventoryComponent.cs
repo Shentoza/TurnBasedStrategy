1b51e392a1615f1dd862f9909039ddab2a325999
street <e.razer@gmx.de>
2016-01-18 17:11:26 +0100

    inventory und weapon prefabs

---
+++ Assets/Scripts/GameLogic/Components/InventoryComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryComponent : MonoBehaviour {
+
+    public WeaponComponent primary;
+    public WeaponComponent secondary;
+    public GameObject equipment;
+    public int amountSmokes;
+    public int amountMolotovs;
+    public int amountMediKits;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameLogic/Components/WeaponComponent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponComponent : MonoBehaviour {
+
+    public bool isPrimary;
+    public int damage;
+    public int rangeMod;
+    public string name;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
